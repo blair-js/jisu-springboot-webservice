288a37eec36bc723f4b5b2d5fc3044e5e15c3afd
blair-js <jih-su@hanmail.net>
2022-07-18 20:56:50 +0900

    Chap 04 : 머스테치(Mustache)로 화면 구성하기 - 게시글 조회/수정/삭제

--- src/main/java/com/jisu/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/jisu/book/springboot/domain/posts/PostsRepository.java
@@ -1,7 +1,13 @@
 package com.jisu.book.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
     //제네릭에 기본 Entity 클래스와 해당 Entity 클래스의 PK 대입
+
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
--- src/main/java/com/jisu/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/jisu/book/springboot/service/posts/PostsService.java
@@ -2,13 +2,16 @@ package com.jisu.book.springboot.service.posts;
 
 import com.jisu.book.springboot.domain.posts.Posts;
 import com.jisu.book.springboot.domain.posts.PostsRepository;
+import com.jisu.book.springboot.web.dto.PostsListResponseDto;
 import com.jisu.book.springboot.web.dto.PostsResponseDto;
 import com.jisu.book.springboot.web.dto.PostsSaveRequestDto;
 import com.jisu.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -36,6 +39,14 @@ public class PostsService {
 
     }
 
+    @Transactional
+    public void delete(Long id){
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
+    }
+
     public PostsResponseDto findById(Long id){
         //아이디 조회 로직
 
@@ -43,5 +54,13 @@ public class PostsService {
                 new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
